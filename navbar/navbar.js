6b5841e087e8027a17b21268d389b08130b2f6db
Adityaprajapati26 <99543597+Adityaprajapati26@users.noreply.github.com>
2022-04-01 20:21:25 +0530

    navbar uploded
    location is left to add//linking

---
+++ navbar/navbar.js
@@ -0,0 +1,41 @@
+const data=()=>{
+    return` <div id="top">
+    <div>
+        <img src="https://media6.ppl-media.com/mediafiles/ecomm/home/1543587824_logo.png" alt="">
+        <p >⭐⭐Get Flat 10% Off up to Rs.150⭐⭐</p>
+    </div>
+    <div>
+       <button id="button1" onclick="window.location.href='https://bnc.lt/m/xLJmQCwBwN?_p=c11434dc9c047af2e21e90ffe9b7'">DOWNLOAD APP</button>
+       <button onclick=" document.getElementById('top').style.display='none'">X</button>
+    </div>
+</div>
+<div id="second">
+ <a href="https://bnc.lt/m/xLJmQCwBwN?_p=c11434dc9c047af2e21e90ffe9b7">Download App</a>    
+</div>
+<div id="navbar">
+    <div>
+    <div id="search"> 
+     <input id="Search" type="text" placeholder="What is you are loking for ?" >
+        
+    </div>
+    <div id="logo">
+        <a href="#">
+        <img src="https://media6.ppl-media.com/static/purplle/img/purplle-logo-1.svg" alt=""></a>
+    </div>
+    <div id="feature">
+        <div>
+            <i class="fa-solid fa-heart" ></i>
+        </div>
+        <div>
+            
+            <i class="fa-regular fa-face-smile"></i>
+        </div>
+        
+        <div><i class="fa-solid fa-cart-arrow-down"></i>
+        </div>
+        
+    </div>
+</div>
+</div>`
+}
+export default data;
